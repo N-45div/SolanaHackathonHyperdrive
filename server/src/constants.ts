0e7452e825f9dfba5b6cc55a49d64591dfebc0e4
Divyansh <divyansh21april@gmail.com>
2023-10-10 16:26:15 -0400

    removed useless directory

--- server/src/constants.ts
+++
@@ -1,7 +0,0 @@
-export const DB_CONNECTION_STRING =
-  process.env.DB_CONNECTION_STRING ||
-  "mongodb://127.0.0.1:27017/password-manager";
-
-export const CORS_ORIGIN = process.env.CORS_ORIGIN || "http://127.0.0.1:3000";
-
-export const COOKIE_DOMAIN = process.env.COOKIE_DOMAIN || "localhost";
